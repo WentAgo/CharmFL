40b5593b7ceacde29c4062bd81db58a4f23f0ad0
Vandlik Viktor <h150209@stud.u-szeged.hu>
2025-04-14 15:50:30 +0200

    .

--- src/main/python/faultloc/Spectra.py
+++ src/main/python/faultloc/Spectra.py
@@ -35,38 +35,25 @@ class Spectra:
                 self.spectrum[str(file)+self.SEPARATOR_CHARACTER+str(code_element)] = {"ef": ef, "ep":ep, "nf":nf,"np":np, "efmod": ef_mod, "epmod" : ep_mod}
 
     def heuristic_analyzer(self,test):
-        #chains from JSON
         project_path = os.path.abspath("callchains.json")
 
         with open(project_path) as file:
-            # print(f"{os.path.dirname(os.path.abspath(projectdir))}/D4J/{project}" + str(bugid) + "heuristics.txt")
             data = file.read()
             call_chains = json.loads(data)
 
         chains = call_chains[test]
-        # Flatten the list of chains and count their occurrences
 
         chain_lengths = len(chains)
 
-        # Rule 1: A test with only one chain of any length is a unit test
         if all([len(elem) == 1 for elem in chains]) and 0 < chain_lengths <= 1:
             return "put"
 
         if all([len(elem) <= 2 for elem in chains]) and chain_lengths > 0:
             return "smt"
 
-        # # Rule 2: If a test has multiple chains but they are consistent in length except for the last method
-        # if len(length_counts) > 1:
-        #     unique_lengths = sorted(length_counts.keys())
-        #     if len(unique_lengths) == 2 and unique_lengths[1] - unique_lengths[0] == 1:
-        #         unit_tests.append(test_name)
-        #         continue
-
         if chain_lengths == 1:
             if all([len(elem) <= 3 for elem in chains]):
-                # Rule 2: Single chain but not longer than 3
                 return "lmc"
-            # Rule 3: Single chain from test
             return "smc"
 
     def get_spectrum(self):
